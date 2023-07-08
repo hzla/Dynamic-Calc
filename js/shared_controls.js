7618ef706f93ccad2f9fd4aadb21c43a5b0a0ec6
TingleyWJ <tingleywj@gmail.com>
2023-07-08 16:58:18 -0400

    Bugfixes Round 2
    - Properly adds Grassy Dash in the movelist.
    - Fixes forme shifting such that the Chosen Set ability will always be chosen first if there is one.
    - Adds support for importing abilities in the "poks" list using npoint.
    - Forces a refresh of the form & abilities when the user clicks on the their imported sets box.

--- js/shared_controls.js
+++ js/shared_controls.js
@@ -757,12 +757,12 @@ $(".forme").change(function () {
 	var chosenSet = pokemonSets && pokemonSets[setName];
 	var greninjaSet = $(this).val().indexOf("Greninja") !== -1;
 	var isAltForme = $(this).val() !== pokemonName;
-	if (isAltForme && abilities.indexOf(altForme.abilities[0]) !== -1 && !greninjaSet) {
+	if (chosenSet) {
+		container.find(".ability").val(chosenSet.ability);
+	} else if (isAltForme && abilities.indexOf(altForme.abilities[0]) !== -1 && !greninjaSet) {
 		container.find(".ability").val(altForme.abilities[0]);
 	} else if (greninjaSet) {
 		$(this).parent().find(".ability");
-	} else if (chosenSet) {
-		container.find(".ability").val(chosenSet.ability);
 	}
 	container.find(".ability").keyup();
 
